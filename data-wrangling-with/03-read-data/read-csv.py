7bb4c8369ffa9711a2bb14c220f8fcccd0e260f7
gbriceno <gbriceno@k12.com>
2019-10-27 15:40:39 -0500

    modify the order of the project

---
+++ data-wrangling-with/03-read-data/read-csv.py
@@ -0,0 +1,23 @@
+import csv
+
+filename = '../data/chp03/data-text.csv'
+
+
+def read_csv(file):
+    csv_file = open(file, "r")
+    reader = csv.reader(csv_file)
+
+    for row in reader:
+        print(row)
+
+
+def read_csv_in_dict(file):
+    csv_file = open(file, "r")
+    reader = csv.DictReader(csv_file)
+
+    for row in reader:
+        print(row)
+
+
+read_csv(filename)
+read_csv_in_dict(filename)
--- data-wrangling-with/README.md
+++ data-wrangling-with/README.md
@@ -4,3 +4,6 @@ Examples based on Data Wrangling with Python
 by: Katharine Jarmul, Jacqueline Kazil
 
 - Data: the data set of the books
+
+Code repository:
+https://github.com/jackiekazil/data-wrangling
